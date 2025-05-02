dca85881dfab2b1fd8e098d9fa2756e4d268b794
DeanGuterman <deangut@gmail.com>
2025-05-02 20:29:43 +0300

    Initial commit

---
+++ src/main/java/com/deanguterman/minidropbox/MiniDropboxApplication.java
@@ -0,0 +1,13 @@
+package com.deanguterman.minidropbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDropboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDropboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deanguterman/minidropbox/MiniDropboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deanguterman.minidropbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDropboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
