0bd53379a0359328f7c28f0422c63246d0cdee70
DeanGuterman <deangut@gmail.com>
2025-05-02 21:19:20 +0300

    Add user registration DTO & restrictions on registration fields

---
+++ src/main/java/com/deanguterman/minidropbox/UserRegistrationRequest.java
@@ -0,0 +1,38 @@
+package com.deanguterman.minidropbox;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+// User Registration DTO
+public class UserRegistrationRequest {
+
+    // Only allow non-blank usernames with a length of between 3 and 20 characters
+    @NotBlank
+    @Size(min=3, max=20)
+    private String username;
+
+    // Only allow non-blank email addresses with a maximum length of 100 characters
+    @NotBlank
+    @Email
+    @Size(max=100)
+    private String email;
+
+    // Only allow non-blank passwords with a length of between 8 and 64 characters
+    @NotBlank
+    @Size(min=8, max=64)
+    private String password;
+
+
+    // Getters
+    public String getEmail(){return email;}
+    public String getUsername(){return username;}
+    public String getPassword(){return password;}
+
+    // Setters
+    public void setEmail(String email){this.email = email;}
+    public void setUsername(String username){this.username = username;}
+    public void setPassword(String password){this.password = password;}
+
+
+}
--- src/main/java/com/deanguterman/minidropbox/UserRepository.java
+++ src/main/java/com/deanguterman/minidropbox/UserRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+// Repository for the User class
 public interface UserRepository extends JpaRepository<User, Long>{
-
     // JPA automatically implements these functions
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
