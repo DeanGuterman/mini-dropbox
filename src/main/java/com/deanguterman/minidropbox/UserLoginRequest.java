57170f04cb2a0a9b80b6926004f83dd8b710b1fa
DeanGuterman <deangut@gmail.com>
2025-05-04 19:08:03 +0300

    Add a user login DTO

--- src/main/java/com/deanguterman/minidropbox/UserLoginRequest.java
+++ src/main/java/com/deanguterman/minidropbox/UserLoginRequest.java
@@ -1,5 +1,18 @@
 package com.deanguterman.minidropbox;
 
+import jakarta.validation.constraints.NotBlank;
+
 // User login DTO
 public class UserLoginRequest {
+
+    private String username;
+    private String password;
+
+    // Getters
+    public String getUsername(){return username;}
+    public String getPassword(){return password;}
+
+    // Setters
+    public void setUsername(String username){this.username = username;}
+    public void setPassword(String password){this.password = password;}
 }
