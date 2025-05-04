38d28a82c5010cfe02ee641855c96f77a796f5e4
DeanGuterman <deangut@gmail.com>
2025-05-04 20:01:20 +0300

    Fully implement the FileEntity class

--- src/main/java/com/deanguterman/minidropbox/FileEntity.java
+++ src/main/java/com/deanguterman/minidropbox/FileEntity.java
@@ -18,4 +18,18 @@ public class FileEntity {
 
     @ManyToOne
     private User user;
+
+    // Getters
+    public String getFilename(){return filename;}
+    public String getPath(){return path;}
+    public Long getSize(){return size;}
+    public LocalDateTime getUploadTime(){return uploadTime;}
+    public User getUser(){return user;}
+
+    // Setters
+    public void setFilename(String filename){this.filename = filename;}
+    public void setPath(String path){this.path = path;}
+    public void setSize(Long size){this.size = size;}
+    public void setUploadTime(LocalDateTime uploadTime){this.uploadTime = uploadTime;}
+    public void setUser(User user){this.user = user;}
 }
