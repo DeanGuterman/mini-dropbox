8e5aee41b20511fe0a6b32b44a97f7979a8dd2c1
DeanGuterman <deangut@gmail.com>
2025-05-04 17:49:50 +0300

    Add a password encoder

--- src/main/java/com/deanguterman/minidropbox/MiniDropboxApplication.java
+++ src/main/java/com/deanguterman/minidropbox/MiniDropboxApplication.java
@@ -2,9 +2,20 @@ package com.deanguterman.minidropbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MiniDropboxApplication {
+
+
+	// Singleton encoder object to inject into other components
+	// @Bean is an annotation that means Spring will manage this
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniDropboxApplication.class, args);
