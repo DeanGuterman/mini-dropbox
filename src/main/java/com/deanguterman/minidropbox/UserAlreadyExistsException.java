95bd99fdcc0437507e11c2d28809404aab7fadaf
DeanGuterman <deangut@gmail.com>
2025-05-04 18:35:08 +0300

    Update comments

--- src/main/java/com/deanguterman/minidropbox/UserAlreadyExistsException.java
+++ src/main/java/com/deanguterman/minidropbox/UserAlreadyExistsException.java
@@ -1,5 +1,6 @@
 package com.deanguterman.minidropbox;
 
+// Custom exception for handling pre-existing emails and password on registration
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(String message){
         super(message);
