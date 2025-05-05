1a7e04213f1226f87b42abdd98a8961afadbc6da
DeanGuterman <deangut@gmail.com>
2025-05-05 18:42:06 +0300

    Implement file download logic

--- src/main/java/com/deanguterman/minidropbox/FileService.java
+++ src/main/java/com/deanguterman/minidropbox/FileService.java
@@ -1,12 +1,18 @@
 package com.deanguterman.minidropbox;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class FileService {
@@ -32,5 +38,17 @@ public class FileService {
         FileEntity newFile = new FileEntity(file.getOriginalFilename(), destination.getAbsolutePath(), file.getSize(), LocalDateTime.now(), user);
         fileRepository.save(newFile);
         return "File uploaded successfully";
+    }
+
+    public Resource downloadFile(Long fileId) throws Exception{
+        Optional<FileEntity> file = fileRepository.findById(fileId);
+        if (file.isPresent()){
+            String filePath = file.get().getPath();
+            File actualFile = new File(filePath);
+            if (!actualFile.exists()) throw new FileNotFoundException("File not found on disk");
+            return new InputStreamResource(new FileInputStream(actualFile));
+        } else {
+            throw new Exception("File not found in database");
+        }
     }
 }
