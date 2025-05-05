cb449802a12857dbaf3add6d9dfba7d90fb987d1
DeanGuterman <deangut@gmail.com>
2025-05-05 18:49:21 +0300

    Implement file download Endpoint

--- src/main/java/com/deanguterman/minidropbox/FileController.java
+++ src/main/java/com/deanguterman/minidropbox/FileController.java
@@ -1,11 +1,10 @@
 package com.deanguterman.minidropbox;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
@@ -33,4 +32,17 @@ public class FileController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found");
         }
     }
+
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<Resource> downloadFIle(@PathVariable Long fileId){
+        try{
+            Resource fileResource = fileService.downloadFile(fileId);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileResource.getFilename() + "\"")
+                    .body(fileResource);
+        } catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
 }
