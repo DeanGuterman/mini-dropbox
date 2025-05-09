2227086a037c0bba54c2fd4ac9162005bc6adb16
DeanGuterman <deangut@gmail.com>
2025-05-09 12:37:59 +0300

    Implement class constructor

--- src/main/java/com/deanguterman/minidropbox/JwtService.java
+++ src/main/java/com/deanguterman/minidropbox/JwtService.java
@@ -1,4 +1,19 @@
 package com.deanguterman.minidropbox;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
 public class JwtService {
+
+    private final Key secretKey;
+    private final long jwtExpirationMilis;
+
+    public JwtService(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long jwtExpirationMilis) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        this.jwtExpirationMilis = jwtExpirationMilis;
+    }
 }
