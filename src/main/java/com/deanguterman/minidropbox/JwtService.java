5af273f17107a167c33f8c32afa6951d639b35bc
DeanGuterman <deangut@gmail.com>
2025-05-09 12:42:39 +0300

    Implement a method to generate tokens

--- src/main/java/com/deanguterman/minidropbox/JwtService.java
+++ src/main/java/com/deanguterman/minidropbox/JwtService.java
@@ -1,10 +1,13 @@
 package com.deanguterman.minidropbox;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
@@ -15,5 +18,14 @@ public class JwtService {
     public JwtService(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long jwtExpirationMilis) {
         this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
         this.jwtExpirationMilis = jwtExpirationMilis;
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMilis))
+                .signWith(secretKey)
+                .compact();
     }
 }
