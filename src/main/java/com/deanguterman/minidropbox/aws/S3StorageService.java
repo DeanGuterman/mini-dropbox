507e6607b031e10882c0a974e1a182ee8b9db2da
DeanGuterman <deangut@gmail.com>
2025-05-13 20:10:45 +0300

    feat(aws): modify downloadfilefroms3 in interface to correctly throw filenotfound exception

--- src/main/java/com/deanguterman/minidropbox/aws/S3StorageService.java
+++ src/main/java/com/deanguterman/minidropbox/aws/S3StorageService.java
@@ -1,10 +1,12 @@
 package com.deanguterman.minidropbox.aws;
 
+import com.deanguterman.minidropbox.exception.FileEmptyException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public interface S3StorageService {
     String uploadFileToS3(MultipartFile file, String key);
-    InputStream downloadFileFromS3(String s3key);
+    InputStream downloadFileFromS3(String s3key) throws FileNotFoundException;
 }
