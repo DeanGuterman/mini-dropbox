510b0716b6ebdebc8c26b479b804b5cc701fb652
DeanGuterman <deangut@gmail.com>
2025-05-13 20:04:44 +0300

    feat(aws): Implement a download from s3 function in s3 storage service

--- src/main/java/com/deanguterman/minidropbox/aws/S3StorageServiceImpl.java
+++ src/main/java/com/deanguterman/minidropbox/aws/S3StorageServiceImpl.java
@@ -1,13 +1,21 @@
 package com.deanguterman.minidropbox.aws;
 
+import com.deanguterman.minidropbox.exception.FileDoesntExistException;
 import com.deanguterman.minidropbox.exception.FileEmptyException;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 public class S3StorageServiceImpl implements S3StorageService {
@@ -38,5 +46,18 @@ public class S3StorageServiceImpl implements S3StorageService {
         }
 
         return key;
+    }
+
+    public InputStream downloadFileFromS3(String s3key) throws FileNotFoundException {
+        try {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(s3key)
+                    .build();
+            ResponseInputStream<GetObjectResponse> s3Object = s3Client.getObject(getObjectRequest);
+            return s3Object;
+        } catch (Exception e) {
+            throw new FileNotFoundException("File not found in S3: " + s3key);
+        }
     }
 }
