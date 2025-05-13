c7496751856ee5bd3fcaaa3d88c7a1790442f614
DeanGuterman <deangut@gmail.com>
2025-05-13 20:07:40 +0300

    feat(aws): add downloadFileFromS3 function to the interface

--- src/main/java/com/deanguterman/minidropbox/aws/S3StorageService.java
+++ src/main/java/com/deanguterman/minidropbox/aws/S3StorageService.java
@@ -2,6 +2,9 @@ package com.deanguterman.minidropbox.aws;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
+
 public interface S3StorageService {
     String uploadFileToS3(MultipartFile file, String key);
+    InputStream downloadFileFromS3(String s3key);
 }
